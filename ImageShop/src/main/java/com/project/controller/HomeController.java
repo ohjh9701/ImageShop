46117bfc39d92973817814d914133a05b2ac71d0
ohjh9701 <ohjh4875@gmail.com>
2026-02-20 15:57:40 +0900

    [생성] 홈 화면 구성

---
+++ ImageShop/src/main/java/com/project/controller/HomeController.java
@@ -0,0 +1,42 @@
+package com.project.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@Controller
+public class HomeController {
+	
+	//private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	//DELETE => DELETE / GET => SELECT / POST => INSERT / PUT => UPDATE
+	
+	@GetMapping("/")
+	public String home(Locale locale, Model model) {
+		
+		log.info("Slf4j 환영합니다. 지역은 " + locale + " 입니다.");
+		//logger.info("logger 환영합니다. 지역은 " + locale + " 입니다.");
+		
+		//현재시간 삽입
+		Date date = new Date();
+		
+		//우리나라 시간과 날짜형식
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		//이 양식에 현재 날짜를 적용할거야
+		String formatterDate = dateFormat.format(date);
+		
+		//화면에 정보를 전달하자(Model)
+		model.addAttribute("serverTime", formatterDate);
+		
+		return "home";
+	}
+}
