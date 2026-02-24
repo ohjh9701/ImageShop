afd6cc0e5a1c94dae4b08775f1145345bebe78b3
ohjh9701 <ohjh4875@gmail.com>
2026-02-24 15:11:40 +0900

    [생성] 쇼핑몰 프로젝트 실습 - 시큐리티 셋팅

--- ImageShop/src/main/java/com/project/controller/CodeDetailController.java
+++ ImageShop/src/main/java/com/project/controller/CodeDetailController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -15,9 +16,6 @@ import com.project.service.CodeDetailService;
 import com.project.service.CodeService;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @Controller
