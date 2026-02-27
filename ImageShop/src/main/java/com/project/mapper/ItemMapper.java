11e538b83b25df0464bfd14865723334fdbf36af
ohjh9701 <ohjh4875@gmail.com>
2026-02-27 15:37:48 +0900

    [생성] 쇼핑몰 프로젝트 실습 - 상품 등록을 위한 초기 셋팅

---
+++ ImageShop/src/main/java/com/project/mapper/ItemMapper.java
@@ -0,0 +1,7 @@
+package com.project.mapper;
+
+public interface ItemMapper {
+	
+	
+	
+}
---
+++ ImageShop/src/main/java/com/project/service/ItemServiceImpl.java
@@ -0,0 +1,10 @@
+package com.project.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemServiceImpl implements ItemService{
+	
+	
+	
+}
